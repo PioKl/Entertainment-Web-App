c1a3d5153b6b9902221b78ddfa8605e35923b997
PioKl <kosiu342@gmail.com>
2024-08-17 21:06:01 +0200

    login update

--- app/pages/account/login/page.tsx
+++ app/pages/account/login/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import styles from "../../../styles/login.module.scss";
+
 export default function Login() {
+  const [emptyForm, setEmptyFrom] = useState(true);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <>
-      <p>login</p>
+      <div className={styles.login}>
+        <h2 className={styles["login__heading"]}>Login</h2>
+        <form onSubmit={handleSubmit} className={styles["login__form"]}>
+          <div className={styles["login__inputs-container"]}>
+            <label htmlFor="email" className={styles["login__label"]}>
+              <input
+                className={styles["login__input"]}
+                pattern="[A-Za-z0-9._+-]+@[A-Za-z0-9 -]+\.[a-z]{2,}"
+                id="email"
+                type="email"
+                placeholder="Email address"
+                required
+              />
+            </label>
+            <label className={styles["login__label"]}>
+              <input
+                className={styles["login__input"]}
+                type="password"
+                placeholder="Password"
+              />
+            </label>
+          </div>
+
+          <button type="submit" className={styles["login__button"]}>
+            Login to your account
+          </button>
+          <p>
+            Don’t have an account?{" "}
+            <Link href="/" className={styles["login__link"]}>
+              Sign Up
+            </Link>
+          </p>
+        </form>
+      </div>
     </>
   );
 }
