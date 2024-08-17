ab987341558d0b477711686fd57eabde90ee87f0
PioKl <kosiu342@gmail.com>
2024-08-17 21:05:32 +0200

    Update

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -7,11 +7,13 @@ export default function Search() {
       <form className={styles.search} action="">
         <div className={styles["search__input-container"]}>
           <SearchIcon className={styles["search__icon"]} />
-          <input
-            type="text"
-            placeholder="Search for movies or TV series"
-            className={styles["search__input"]}
-          />
+          <label className={styles["search__input-label"]}>
+            <input
+              type="text"
+              placeholder="Search for movies or TV series"
+              className={styles["search__input"]}
+            />
+          </label>
         </div>
         <button className={styles["search__button-submit"]} type="submit">
           Submit
