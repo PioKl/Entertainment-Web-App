a1e1b0f6f1856a0f3eb26a6f9f3f7e76b7659d1a
PioKl <kosiu342@gmail.com>
2024-08-20 22:22:52 +0200

    Login update

--- app/pages/account/login/page.tsx
+++ app/pages/account/login/page.tsx
@@ -2,11 +2,9 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import styles from "../../../styles/login.module.scss";
+import styles from "../../../styles/auth.module.scss";
 
 export default function Login() {
-  //const [emptyForm, setEmptyFrom] = useState(true);
-
   const [errors, setErrors] = useState({
     email: false,
     password: false,
@@ -49,53 +47,56 @@ export default function Login() {
   };
   return (
     <>
-      <div className={styles.login}>
-        <h2 className={styles["login__heading"]}>Login</h2>
-        <form onSubmit={handleSubmit} className={styles["login__form"]}>
-          <div className={styles["login__inputs-container"]}>
-            <label htmlFor="email" className={styles["login__label"]}>
+      <div className={styles.auth}>
+        <h2 className={styles["auth__heading"]}>Login</h2>
+        <form onSubmit={handleSubmit} className={styles["auth__form"]}>
+          <div className={styles["auth__inputs-container"]}>
+            <label htmlFor="email" className={styles["auth__label"]}>
               <input
                 value={values.email}
                 onChange={(e) =>
                   setValues({ ...values, email: e.target.value })
                 }
-                className={styles["login__input"]}
+                className={styles["auth__input"]}
                 id="email"
                 type="email"
                 placeholder="Email address"
                 autoComplete="username"
               />
               {errors.email && (
-                <span className={styles["login__error"]}>
+                <span className={styles["auth__error"]}>
                   {messages.errorMessage}
                 </span>
               )}
             </label>
-            <label className={styles["login__label"]}>
+            <label className={styles["auth__label"]}>
               <input
                 value={values.password}
                 onChange={(e) =>
                   setValues({ ...values, password: e.target.value })
                 }
-                className={styles["login__input"]}
+                className={styles["auth__input"]}
                 type="password"
                 placeholder="Password"
                 autoComplete="current-password"
               />
               {errors.password && (
-                <span className={styles["login__error"]}>
+                <span className={styles["auth__error"]}>
                   {messages.errorMessage}
                 </span>
               )}
             </label>
           </div>
 
-          <button type="submit" className={styles["login__button"]}>
+          <button type="submit" className={`btn ${styles["auth__button"]}`}>
             Login to your account
           </button>
           <p>
             Don’t have an account?{" "}
-            <Link href="/" className={styles["login__link"]}>
+            <Link
+              href="/pages/account/register"
+              className={styles["auth__link"]}
+            >
               Sign Up
             </Link>
           </p>
