0d5ef1f1e7963a319c4c7711a4525d1fc192ddd6
PioKl <kosiu342@gmail.com>
2024-08-24 20:54:42 +0200

    Component update

--- app/components/Trending.tsx
+++ app/components/Trending.tsx
@@ -13,44 +13,61 @@ const fetcher = (url: string) =>
 
 export default function Trending() {
   const { data: movies, error } = useSWR(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
+    `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
     fetcher
   );
 
   return (
     <>
       <section className={styles.trending}>
-        <h2 className={styles["trending__heading"]}>Trending</h2>
-        <div className={styles["trending__swiper"]}>
-          {movies ? (
-            <Swiper
-              grabCursor={true}
-              slidesPerView={1.5}
-              spaceBetween={16}
-              breakpoints={{
-                1110: {
-                  slidesPerView: 2.5,
-                  spaceBetween: 40,
-                },
-              }}
-              centeredSlides={false}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Pagination]}
-            >
-              {movies.map((movie: any) => (
-                <SwiperSlide key={movie.id}>
-                  <CardTrending movie={movie} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          ) : (
-            <Loader />
-          )}
+        <div className={styles["trending__container"]}>
+          <h2 className={styles["trending__heading"]}>Trending</h2>
+          <div className={styles["trending__swiper"]}>
+            {movies ? (
+              <Swiper
+                grabCursor={true}
+                slidesPerView={1.5}
+                spaceBetween={16}
+                breakpoints={{
+                  600: {
+                    slidesPerView: 2.5,
+                    spaceBetween: 16,
+                  },
+                  768: {
+                    slidesPerView: 1.5,
+                    spaceBetween: 40,
+                  },
+                  800: {
+                    slidesPerView: 2.5,
+                    spaceBetween: 40,
+                  },
+                  1110: {
+                    slidesPerView: 2.5,
+                    spaceBetween: 40,
+                  },
+                  1441: {
+                    slidesPerView: 3.5,
+                    spaceBetween: 40,
+                  },
+                }}
+                centeredSlides={false}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[Pagination]}
+              >
+                {movies.map((movie: any) => (
+                  <SwiperSlide key={movie.id}>
+                    <CardTrending movie={movie} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            ) : (
+              <Loader />
+            )}
+          </div>
         </div>
       </section>
-      <h2>Test</h2>
     </>
   );
 }
