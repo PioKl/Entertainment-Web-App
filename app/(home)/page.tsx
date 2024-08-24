e95abfcf79b2253e606e25a6175c208eb6005b5d
PioKl <kosiu342@gmail.com>
2024-08-24 20:53:49 +0200

    Add Popular component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,12 +2,14 @@
 import styles from "../styles/page.module.scss";
 import Search from "../components/Search";
 import Trending from "../components/Trending";
+import Popular from "../components/Popular";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Search />
       <Trending />
+      <Popular />
     </main>
   );
 }
