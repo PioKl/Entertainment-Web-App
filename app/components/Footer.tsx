21b5200f82ab4cc61b20f10e6faaf503f446d0cc
PioKl <kosiu342@gmail.com>
2024-08-27 16:12:18 +0200

    Footer update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import LogoIcon from "../images/tmdb-logo.svg";
+import styles from "../styles/footer.module.scss";
+
 export default function Footer() {
   return (
-    <footer className="footer">
-      <span>Footer</span>
+    <footer className={styles.footer}>
+      <div className={styles["footer__link-container"]}>
+        <span>Powered by</span>
+        <Link
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          className={styles["footer__tmdb-link"]}
+        >
+          <LogoIcon
+            className={styles["footer__tmdb-logo"]}
+            alt="The Movie DB"
+          />
+        </Link>
+      </div>
     </footer>
   );
 }
