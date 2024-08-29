17b054a2f76739952427bc46c215898b0ecb8375
PioKl <kosiu342@gmail.com>
2024-08-29 20:05:37 +0200

    Component update (title, name, mediaType, poster, backdrop)

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -5,9 +5,10 @@ import MovieIcon from "../images/icon-category-movie.svg";
 
 interface CardProps {
   movie: any;
+  mediaType: "dynamic" | "movie" | "tv";
 }
 
-const CardTrending: React.FC<CardProps> = ({ movie }) => {
+const Card: React.FC<CardProps> = ({ movie, mediaType }) => {
   return (
     <div className={styles["card"]}>
       <div
@@ -16,15 +17,19 @@ const CardTrending: React.FC<CardProps> = ({ movie }) => {
           position: "relative",
         }}
       >
-        <Image
-          src={`https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`}
-          alt={movie.title}
-          className={styles["card__image"]}
-          fill
-          sizes="(max-width: 768px) 100vw, 33vw"
-          style={{ objectFit: "cover" }}
-          priority={true}
-        />
+        {(movie.backdrop_path || movie.poster_path) && (
+          <Image
+            src={`https://image.tmdb.org/t/p/w780/${
+              movie.backdrop_path ? movie.backdrop_path : movie.poster_path
+            }`}
+            alt={movie.title || movie.name}
+            className={styles["card__image"]}
+            fill
+            sizes="(max-width: 768px) 100vw, 33vw"
+            style={{ objectFit: "cover" }}
+            priority={true}
+          />
+        )}
         {/* w przyszlości z active className={`${styles.card__bookmark} ${styles['--active']}`} */}
         <button type="button" className={`${styles.card__bookmark}`}>
           <BookMarkIcon className={styles["card__bookmark-icon"]} />
@@ -33,19 +38,28 @@ const CardTrending: React.FC<CardProps> = ({ movie }) => {
       <div className={styles["card__quick-info"]}>
         <ul className={styles["card__info-items-list"]}>
           <li className={styles["card__info-list-item"]}>
-            {movie.release_date.substring(0, 4)}
+            {movie.release_date
+              ? movie.release_date && movie.release_date.substring(0, 4)
+              : movie.first_air_date && movie.first_air_date.substring(0, 4)}
           </li>
           <li className={styles["card__info-list-item"]}>
             <MovieIcon />
           </li>
           <li className={styles["card__info-list-item"]}>
-            <span>Movie</span>
+            <span className={styles["card__media-type"]}>
+              {mediaType === "dynamic" &&
+                (movie.media_type === "movie" ? "Movie" : "TV Series")}
+              {mediaType === "movie" && "Movie"}
+              {mediaType === "tv" && "TV Series"}
+            </span>
           </li>
         </ul>
-        <span className={styles["card__title"]}>{movie.title}</span>
+        <span className={styles["card__title"]}>
+          {movie.title || movie.name}
+        </span>
       </div>
     </div>
   );
 };
 
-export default CardTrending;
+export default Card;
