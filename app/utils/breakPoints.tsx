6629c58379f3267d9f6ab6a61d93f9abc33f0de8
PioKl <kosiu342@gmail.com>
2024-08-30 19:28:58 +0200

    Add breakPoints

---
+++ app/utils/breakPoints.tsx
@@ -0,0 +1,5 @@
+export const breakpoints = {
+  bpTablet: 48, // to jest w em, po konwersji emToPixels będzie to 768px, poniżej analogicznie
+  bpDesktopSmall: 69.375, // 1110px
+  bpDesktop: 90, // 1440px
+};
