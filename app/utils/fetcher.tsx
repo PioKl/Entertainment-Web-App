b42a1f211437cedba918e8474a6da77faddef39b
PioKl <kosiu342@gmail.com>
2024-09-03 16:13:53 +0200

    Add fetcher

---
+++ app/utils/fetcher.tsx
@@ -0,0 +1,24 @@
+export const fetcher = (url: string) =>
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => data.results || []);
+
+//data.results || [] jeśli data results nie istnieje zwróci pustą tablicę
+
+export const fetcherSearch = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Response Error");
+    }
+    const data = await response.json();
+    return {
+      results: data.results || [],
+      totalResults: data.total_results || 0,
+      totalPages: data.total_pages || 0,
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
