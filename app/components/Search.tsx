3712240f773608c67b4a83ed50fbb35f2a688e53
PioKl <kosiu342@gmail.com>
2024-09-03 16:11:22 +0200

    Search update (routing)

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,10 +1,26 @@
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import styles from "../styles/search.module.scss";
 import SearchIcon from "../images/icon-search.svg";
 
 export default function Search() {
+  const router = useRouter();
+  const [query, setQuery] = useState("");
+  const path = "/search/";
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (query.length === 0) {
+      return;
+    } else {
+      router.push(`${path}${query.trim()}?page=1`);
+      setQuery("");
+    }
+  };
+
   return (
     <>
-      <form className={styles.search} action="">
+      <form className={styles.search} onSubmit={handleSearch}>
         <div className={styles["search__input-container"]}>
           <SearchIcon className={styles["search__icon"]} />
           <label className={styles["search__input-label"]}>
@@ -12,6 +28,8 @@ export default function Search() {
               type="text"
               placeholder="Search for movies or TV series"
               className={styles["search__input"]}
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
             />
           </label>
         </div>
