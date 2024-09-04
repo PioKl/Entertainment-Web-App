fd3a8fed16779fa026705ee3b7967e6d5926214a
PioKl <kosiu342@gmail.com>
2024-09-04 18:42:06 +0200

    Add search and new topics

--- app/(home)/tv/page.tsx
+++ app/(home)/tv/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+import styles from "@/app/styles/page.module.scss";
+import Search from "@/app/components/Search";
+import TopicSwiper from "@/app/components/TopicSwiper";
+import Topic from "@/app/components/Topic";
+import { endpoints } from "@/app/utils/endpoints";
+
 export default function Tv() {
   return (
-    <main>
-      <div>
-        <h2>Tv</h2>
-      </div>
+    <main className={styles.main}>
+      <Search searchType="tv" />
+      <TopicSwiper
+        topicName="Trending"
+        apiUrl={endpoints.trendingTv}
+        mediaType="tv"
+      />
+      <Topic topicName="Popular" apiUrl={endpoints.popularTv} mediaType="tv" />
     </main>
   );
 }
