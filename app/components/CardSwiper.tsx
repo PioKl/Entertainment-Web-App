053afe67321d95c1914b0a53ef1bd75a723ea645
PioKl <kosiu342@gmail.com>
2024-09-04 18:40:20 +0200

    Add new props

--- app/components/CardSwiper.tsx
+++ app/components/CardSwiper.tsx
@@ -1,13 +1,19 @@
 import styles from "../styles/cardSwiper.module.scss";
 import Image from "next/image";
 import MovieIcon from "../images/icon-category-movie.svg";
+import TvIcon from "@/app/images/icon-category-tv.svg";
 import BookMarkIcon from "../images/icon-bookmark-empty.svg";
 
 interface CardSwiperProps {
   movie: any;
+  mediaType?: "dynamic" | "movie" | "tv";
 }
 
-const CardSwiper: React.FC<CardSwiperProps> = ({ movie }) => {
+const CardSwiper: React.FC<CardSwiperProps> = ({
+  movie,
+  mediaType = "dynamic",
+}) => {
+  console.log(movie);
   return (
     <div
       className={styles["card-swiper"]}
@@ -20,7 +26,7 @@ const CardSwiper: React.FC<CardSwiperProps> = ({ movie }) => {
     >
       <Image
         src={`https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`}
-        alt={movie.title}
+        alt={movie.title || movie.name}
         className={styles["card-swiper__image"]}
         fill
         sizes="(max-width: 768px) 100vw, 33vw"
@@ -36,16 +42,25 @@ const CardSwiper: React.FC<CardSwiperProps> = ({ movie }) => {
       <div className={styles["card-swiper__quick-info"]}>
         <ul className={styles["card-swiper__info-items-list"]}>
           <li className={styles["card-swiper__info-list-item"]}>
-            {movie.release_date.substring(0, 4)}
+            {movie.release_date
+              ? movie.release_date && movie.release_date.substring(0, 4)
+              : movie.first_air_date && movie.first_air_date.substring(0, 4)}
           </li>
           <li className={styles["card-swiper__info-list-item"]}>
-            <MovieIcon />
+            {mediaType === "dynamic" && <MovieIcon />}
+            {mediaType === "movie" && <MovieIcon />}
+            {mediaType === "tv" && <TvIcon />}
           </li>
           <li className={styles["card-swiper__info-list-item"]}>
-            <span>Movie</span>
+            {mediaType === "dynamic" &&
+              (movie.media_type === "movie" ? "Movie" : "TV Series")}
+            {mediaType === "movie" && "Movie"}
+            {mediaType === "tv" && "TV Series"}
           </li>
         </ul>
-        <span className={styles["card-swiper__title"]}>{movie.title}</span>
+        <span className={styles["card-swiper__title"]}>
+          {movie.title || movie.name}
+        </span>
       </div>
     </div>
   );
