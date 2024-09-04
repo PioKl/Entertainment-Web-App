54f2382a9b1d469a6418b6a0b07860d3116cf12b
PioKl <kosiu342@gmail.com>
2024-09-04 18:36:24 +0200

    Add new endpoints

--- app/utils/endpoints.tsx
+++ app/utils/endpoints.tsx
@@ -1,7 +1,11 @@
 export const endpoints: {
-  popularMovies: string;
   trendingMovies: string;
+  popularMovies: string;
+  trendingTv: string;
+  popularTv: string;
 } = {
   trendingMovies: `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
   popularMovies: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
+  trendingTv: `https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
+  popularTv: `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
 };
