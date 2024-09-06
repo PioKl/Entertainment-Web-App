fadc724282f5cb3fc1d9040947df07655119083b
PioKl <kosiu342@gmail.com>
2024-09-06 19:33:30 +0200

    Add GenreFilter

--- app/(home)/tv/page.tsx
+++ app/(home)/tv/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import styles from "@/app/styles/page.module.scss";
+import { endpoints } from "@/app/utils/endpoints";
 import Search from "@/app/components/Search";
 import TopicSwiper from "@/app/components/TopicSwiper";
 import Topic from "@/app/components/Topic";
-import { endpoints } from "@/app/utils/endpoints";
+import GenreFilter from "@/app/components/GenreFilter";
 
 export default function Tv() {
   return (
     <main className={styles.main}>
       <Search searchType="tv" />
+      <GenreFilter filterType="tv" />
       <TopicSwiper
         topicName="Trending"
         apiUrl={endpoints.trendingTv}
