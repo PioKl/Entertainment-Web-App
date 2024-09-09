dc8086e5f01c109fe3f6569532a7e5ec1d2e794e
PioKl <kosiu342@gmail.com>
2024-09-09 18:35:32 +0200

    Route update

--- app/api/movies/[id]/route.tsx
+++ app/api/movies/[id]/route.tsx
@@ -1,25 +1,20 @@
 import { NextResponse } from "next/server";
+import { getGenreMoviesByCategoryWithPage } from "@/app/utils/endpoints";
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
+  const { searchParams } = new URL(request.url);
   const { id } = params;
-  const url = new URL(request.url);
-  const page = url.searchParams.get("page") || "1"; //Domyślnie strona pierwsza
+  const page = searchParams.get("page") || "1"; //Domyślnie strona pierwsza
 
   if (!id) {
     return NextResponse.json({ error: "ID is missing" }, { status: 400 });
   }
 
   try {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${
-        process.env.NEXT_PUBLIC_API_KEY
-      }&include_adult=false&with_genres=${encodeURIComponent(
-        id
-      )}}&language=en-US&sort_by=popularity.desc&page=${page}`
-    );
+    const response = await fetch(getGenreMoviesByCategoryWithPage(id, page));
     const data = await response.json();
     return NextResponse.json(data);
   } catch (error) {
