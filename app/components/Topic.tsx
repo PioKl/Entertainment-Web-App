deb226689315d5abb10bbc70d23edba6efecd7d1
PioKl <kosiu342@gmail.com>
2024-09-12 16:48:05 +0200

    Component update

--- app/components/Topic.tsx
+++ app/components/Topic.tsx
@@ -1,4 +1,5 @@
 import useSWR from "swr";
+import { useRouter } from "next/navigation";
 import { fetcher } from "../utils/fetcher";
 import Card from "./Card";
 import Error from "@/app/components/Error";
@@ -8,20 +9,44 @@ import styles from "../styles/cardsTopic.module.scss";
 interface TopicProps {
   topicName: string;
   apiUrl: string;
+  topicType?: "moviePopular" | "tvPopular"; //?: opcjonalny props
   mediaType?: "dynamic" | "movie" | "tv";
 }
+
+const topicSeeMorePath = {
+  moviePopular: "/movies/popular/1",
+  tvPopular: "/tv/popular/1",
+};
 
 const Topic: React.FC<TopicProps> = ({
   topicName,
   apiUrl,
+  topicType,
   mediaType = "dynamic",
 }) => {
+  const router = useRouter();
   const { data: movies, error } = useSWR(apiUrl, fetcher);
+
+  const handleSeeMoreButton = () => {
+    {
+      topicType && router.push(topicSeeMorePath[topicType]);
+    }
+  };
 
   return (
     <>
       <section className={styles.cardsTopic}>
-        <h2 className={styles["cardsTopic__title"]}>{topicName}</h2>
+        <div className={styles["cardsTopic__title-container"]}>
+          <h2 className={styles["cardsTopic__title"]}>{topicName}</h2>
+          {!error && topicType && (
+            <button
+              className={`btn ${styles["cardsTopic__more-button"]}`}
+              onClick={handleSeeMoreButton}
+            >
+              See More
+            </button>
+          )}
+        </div>
 
         {error ? (
           <Error errorType="error" siteType="static" />
