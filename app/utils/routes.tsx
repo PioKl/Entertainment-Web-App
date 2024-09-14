c2976d5cd8574aa9b1e18157dc4f6f5bd67dede8
PioKl <kosiu342@gmail.com>
2024-09-14 16:25:14 +0200

    Add new route

--- app/utils/routes.tsx
+++ app/utils/routes.tsx
@@ -1,5 +1,33 @@
 import { NextResponse } from "next/server";
 
+export function getMediaBySearch(
+  fetchFunction: (query: string, page: string) => string
+) {
+  return async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const query = searchParams.get("query");
+    const page = searchParams.get("page") || "1"; //Domyślnie strona pierwsza
+
+    if (!query) {
+      return NextResponse.json(
+        { error: "Query parameter is missing" },
+        { status: 400 }
+      );
+    }
+    try {
+      const response = await fetch(fetchFunction(query, page));
+      const data = await response.json();
+
+      return NextResponse.json(data);
+    } catch (error) {
+      return NextResponse.json(
+        { error: "Error fetching data from TMDb" },
+        { status: 500 }
+      );
+    }
+  };
+}
+
 export function getMediaByTopicType(fetchFunction: (page: string) => string) {
   //fetchFunction - funkcja przyjmuje stringa w postaci parametru page i zwraca stringa
   return async function GET(
