76b7883e50fee2a0298b3c705dba56cc9e29fae0
PioKl <kosiu342@gmail.com>
2024-09-15 20:00:38 +0200

    Route update

--- app/api/search/[query]/route.tsx
+++ app/api/search/[query]/route.tsx
@@ -1,26 +1,4 @@
-import { NextResponse } from "next/server";
+import { getMediaBySearch } from "@/app/utils/routes";
 import { getSearchAll } from "@/app/utils/endpoints";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const query = searchParams.get("query");
-  const page = searchParams.get("page") || "1"; //Domyślnie strona pierwsza
-
-  if (!query) {
-    return NextResponse.json(
-      { error: "Query parameter is missing" },
-      { status: 400 }
-    );
-  }
-  try {
-    const response = await fetch(getSearchAll(query, page));
-    const data = await response.json();
-
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Error fetching data from TMDb" },
-      { status: 500 }
-    );
-  }
-}
+export const GET = getMediaBySearch(getSearchAll);
