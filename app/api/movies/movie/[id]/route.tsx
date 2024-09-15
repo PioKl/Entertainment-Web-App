0cb22ea7f415e97973361cb4b735be9dede1c071
PioKl <kosiu342@gmail.com>
2024-09-15 19:57:54 +0200

    Route update

--- app/api/movies/movie/[id]/route.tsx
+++ app/api/movies/movie/[id]/route.tsx
@@ -1,24 +1,4 @@
-import { getMovie } from "@/app/utils/endpoints";
-import { getMovieCredits } from "@/app/utils/endpoints";
-import { NextResponse } from "next/server";
+import { getMediaDetails } from "@/app/utils/routes";
+import { getMovie, getMovieCredits } from "@/app/utils/endpoints";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const id = params.id;
-
-  try {
-    const [media, crew] = await Promise.all([
-      fetch(getMovie(id)).then((res) => res.json()),
-      fetch(getMovieCredits(id)).then((res) => res.json()),
-    ]);
-
-    return NextResponse.json({ media, crew });
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Error fetching data from TMDb" },
-      { status: 500 }
-    );
-  }
-}
+export const GET = getMediaDetails(getMovie, getMovieCredits);
