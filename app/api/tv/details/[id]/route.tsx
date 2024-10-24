48e0184e1eb40299e1ea49c26b5c820a13350452
PioKl <kosiu342@gmail.com>
2024-10-24 19:30:16 +0200

    route update

--- app/api/tv/details/[id]/route.tsx
+++ app/api/tv/details/[id]/route.tsx
@@ -1,4 +1,4 @@
 import { getMediaDetails } from "@/app/utils/routes";
-import { getTv, getTvCredits } from "@/app/utils/endpoints";
+import { getTv, getTvCredits, getTvVideo } from "@/app/utils/endpoints";
 
-export const GET = getMediaDetails(getTv, getTvCredits);
+export const GET = getMediaDetails(getTv, getTvCredits, getTvVideo);
