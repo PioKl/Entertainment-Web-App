b8ff1e149f615498ebee8364ecd36fe6d3e427a4
PioKl <kosiu342@gmail.com>
2024-11-19 19:11:27 +0100

    Routes update

--- app/utils/routes.tsx
+++ app/utils/routes.tsx
@@ -1,28 +1,17 @@
 import { NextResponse } from "next/server";
 
 export function getMediaBySearch(
-  fetchFunction: (query: string, page: string, mediaType: string) => string
+  fetchFunction: (query: string, page: string) => string
 ) {
   return async function GET(
     request: Request,
     { params }: { params: { query: string } }
   ) {
+    const query = params.query; // params.query, ponieważ Next.js poprawnie mapuje parametry
     const url = new URL(request.url);
-    const pathname = url.pathname;
-
-    // Ustawienie mediaType na podstawie ścieżki
-    let mediaType = "all";
-    if (pathname.includes("/movies/")) {
-      mediaType = "movie";
-    } else if (pathname.includes("/tv/")) {
-      mediaType = "tv";
-    }
-
-    const query = params.query; // Parametr query z dynamicznej ścieżki
-    const page = url.searchParams.get("page") || "1"; // Strona 1, jeśli brak
+    const page = url.searchParams.get("page") || "1"; // Domyślnie strona 1, jeśli brak
 
     console.log("Query:", query);
-    console.log("Media Type:", mediaType);
     console.log("Page:", page);
 
     if (!query) {
@@ -31,9 +20,8 @@ export function getMediaBySearch(
         { status: 400 }
       );
     }
-
     try {
-      const response = await fetch(fetchFunction(query, page, mediaType));
+      const response = await fetch(fetchFunction(query, page));
       const data = await response.json();
 
       return NextResponse.json(data);
@@ -45,6 +33,35 @@ export function getMediaBySearch(
     }
   };
 }
+
+//dla localhosta
+/* export function getMediaBySearch(
+  fetchFunction: (query: string, page: string) => string
+) {
+  return async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const query = searchParams.get("query");
+    const page = searchParams.get("page") || "1"; //Domyślnie strona pierwsza
+
+    if (!query) {
+      return NextResponse.json(
+        { error: "Query parameter is missing" },
+        { status: 400 }
+      );
+    }
+    try {
+      const response = await fetch(fetchFunction(query, page));
+      const data = await response.json();
+
+      return NextResponse.json(data);
+    } catch (error) {
+      return NextResponse.json(
+        { error: "Error fetching data from TMDb" },
+        { status: 500 }
+      );
+    }
+  };
+} */
 
 export function getMediaByTopicType(fetchFunction: (page: string) => string) {
   //fetchFunction - funkcja przyjmuje stringa w postaci parametru page i zwraca stringa
