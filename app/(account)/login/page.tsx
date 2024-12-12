7e9fc6124c904ea7d2fb3e6ded5554dff65cefbf
PioKl <kosiu342@gmail.com>
2024-12-12 15:34:01 +0100

    Login functionality update

--- app/(account)/login/page.tsx
+++ app/(account)/login/page.tsx
@@ -1,47 +1,91 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { useState } from "react";
 import styles from "../../styles/auth.module.scss";
 
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
 export default function Login() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      router.push("/"); // Przekierowanie na stronę główną, jeśli użytkownik jest zalogowany
+    }
+  }, [router]);
+
   const [errors, setErrors] = useState({
-    email: false,
-    password: false,
+    emailEmpty: false,
+    passwordEmpty: false,
+    wrongEmailOrPassword: false,
   });
 
   const [values, setValues] = useState({
-    email: "",
-    password: "",
+    emailInput: "",
+    passwordInput: "",
   });
 
   const messages = {
     errorMessage: "Can’t be empty",
+    wrongEmailOrPasswordMessage: "Wrong email or password",
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(values.email);
 
     const newErrors = {
-      email: values.email === "",
-      password: values.password === "",
+      emailEmpty: values.emailInput === "",
+      passwordEmpty: values.passwordInput === "",
     };
 
     // Ustawienie błędów w postaci true i false
-    if (newErrors.email || newErrors.password) {
-      setErrors(newErrors);
+    if (newErrors.emailEmpty || newErrors.passwordEmpty) {
+      setErrors((prevErrors) => ({
+        ...prevErrors, // zachowuje aktualne wartości poprzednich błędów
+        emailEmpty: newErrors.emailEmpty,
+        passwordEmpty: newErrors.passwordEmpty,
+      }));
     }
     //Jeśli nie ma błędów, będzie wysłany endpoint
     else {
-      setErrors({
-        email: false,
-        password: false,
-      });
-      setValues({
-        email: "",
-        password: "",
-      });
+      try {
+        const response = await apiClient.post("/api/auth/login", {
+          userEmail: values.emailInput,
+          password: values.passwordInput,
+        });
+
+        localStorage.setItem("token", response.data.token); // Przechowuj token w localStorage
+        setErrors((prevErrors) => ({
+          ...prevErrors, // zachowuje aktualne wartości poprzednich błędów
+          wrongEmailOrPassword: false,
+        }));
+        router.push("/");
+      } catch (error) {
+        if (axios.isAxiosError(error) && error.response) {
+          if (error.response.status === 400) {
+            console.log("Wrong email or password");
+            setErrors((prevErrors) => ({
+              ...prevErrors, // zachowuje aktualne wartości poprzednich błędów
+              wrongEmailOrPassword: true,
+            }));
+          } else {
+            console.error("Unexpected error:", error);
+          }
+        }
+      }
+
+      setErrors((prevErrors) => ({
+        ...prevErrors, // zachowuje aktualne wartości poprzednich błędów
+        emailEmpty: false,
+        passwordEmpty: false,
+      }));
+
       console.log("Send");
     }
   };
@@ -53,9 +97,9 @@ export default function Login() {
           <div className={styles["auth__inputs-container"]}>
             <label htmlFor="email" className={styles["auth__label"]}>
               <input
-                value={values.email}
+                value={values.emailInput}
                 onChange={(e) =>
-                  setValues({ ...values, email: e.target.value })
+                  setValues({ ...values, emailInput: e.target.value })
                 }
                 className={styles["auth__input"]}
                 id="email"
@@ -63,7 +107,7 @@ export default function Login() {
                 placeholder="Email address"
                 autoComplete="username"
               />
-              {errors.email && (
+              {errors.emailEmpty && (
                 <span className={styles["auth__error"]}>
                   {messages.errorMessage}
                 </span>
@@ -71,21 +115,30 @@ export default function Login() {
             </label>
             <label className={styles["auth__label"]}>
               <input
-                value={values.password}
+                value={values.passwordInput}
                 onChange={(e) =>
-                  setValues({ ...values, password: e.target.value })
+                  setValues({ ...values, passwordInput: e.target.value })
                 }
                 className={styles["auth__input"]}
                 type="password"
                 placeholder="Password"
                 autoComplete="current-password"
               />
-              {errors.password && (
+              {errors.passwordEmpty && (
                 <span className={styles["auth__error"]}>
                   {messages.errorMessage}
                 </span>
               )}
             </label>
+            {!errors.emailEmpty &&
+              !errors.passwordEmpty &&
+              errors.wrongEmailOrPassword && (
+                <span
+                  className={`${styles["auth__error"]} ${styles["--wrongEmailOrPassword"]}`}
+                >
+                  {messages.wrongEmailOrPasswordMessage}
+                </span>
+              )}
           </div>
 
           <button type="submit" className={`btn ${styles["auth__button"]}`}>
