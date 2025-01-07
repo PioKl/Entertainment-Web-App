2faba19c603158c49a14b7959cbbbba57c0ed119
PioKl <kosiu342@gmail.com>
2025-01-07 16:52:17 +0100

    Images domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // Opcje konfiguracji
+  images: {
+    domains: ["entertainment-web-app-backend-three.vercel.app"], // Domena, z której będą ładowane obrazy
+  },
+};
 
 export default nextConfig;
