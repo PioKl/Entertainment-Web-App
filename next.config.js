00563e242415cf0493736a6aa0497f61eaf81996
PioKl <kosiu342@gmail.com>
2026-01-21 15:03:37 +0100

    domain change

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
+      rule.test?.test?.(".svg"),
     );
 
     config.module.rules.push(
@@ -23,7 +23,7 @@ module.exports = {
         issuer: fileLoaderRule.issuer,
         resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
         use: ["@svgr/webpack"],
-      }
+      },
     );
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
@@ -39,7 +39,7 @@ module.exports = {
         pathname: "/**",
       },
     ],
-    domains: ["entertainment-web-app-backend-three.vercel.app"], // Domena do ładowania obrazów z backendu
+    domains: ["entertainment-web-app-backend-ashy.vercel.app"], // Domena do ładowania obrazów z backendu
   },
 
   // ...other config
