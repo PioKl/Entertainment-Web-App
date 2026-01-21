14ef459adad26d69edc332d06d96157d4a5a7268
PioKl <kosiu342@gmail.com>
2026-01-21 15:52:15 +0100

    page update

--- app/(home)/movies/[id]/page.tsx
+++ app/(home)/movies/[id]/page.tsx
@@ -1,24 +1,5 @@
-"use client";
-import styles from "@/app/styles/page.module.scss";
-import Search from "@/app/components/Search";
-import GenreFilter from "@/app/components/GenreFilter";
-import GenreFilterResults from "@/app/components/GenreFilterResults";
+import MoviesGenreResult from "./MoviesGenreResult";
 
-type MoviesPageParams = {
-  id: string;
-};
-
-interface MoviesGenreResultProps {
-  params: MoviesPageParams;
+export default function Page({ params }: { params: { id: string } }) {
+  return <MoviesGenreResult params={params} />;
 }
-
-const MoviesGenreResult = ({ params }: MoviesGenreResultProps) => {
-  return (
-    <main className={styles.main}>
-      <Search searchType="movie" />
-      <GenreFilter filterType="movie" />
-      <GenreFilterResults params={params} filterType="movie" />
-    </main>
-  );
-};
-export default MoviesGenreResult;
